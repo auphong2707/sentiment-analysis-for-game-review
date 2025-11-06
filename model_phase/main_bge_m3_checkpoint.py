8b7a8303460de8986962920e565d70b66093d59f
Bùi Văn Huy <buivanhuy0701@gmail.com>
2025-11-06 07:33:43 +0700

    Update main_bge_m3_checkpoint.py

--- model_phase/main_bge_m3_checkpoint.py
+++ model_phase/main_bge_m3_checkpoint.py
@@ -404,7 +404,7 @@ class BGEM3SentimentClassifier:
                 C=self.C,
                 gamma=self.gamma,
                 kernel=self.kernel,
-                max_iter=50000,
+                max_iter=32000,
                 random_state=self.random_state,
                 verbose=True  # Will print progress during training
             )
