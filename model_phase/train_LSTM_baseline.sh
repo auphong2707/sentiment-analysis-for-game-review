95cdaa3fd36fae918ff13632a6c92b8cf67f9cac
Veil <65228307+auphong2707@users.noreply.github.com>
2025-12-23 15:22:19 +0700

    Up the LSTM epoches

--- model_phase/train_LSTM_baseline.sh
+++ model_phase/train_LSTM_baseline.sh
@@ -9,7 +9,7 @@ set -e  # Exit on error
 readonly EMBED_DIM=128
 readonly HIDDEN_DIM=128
 readonly BATCH_SIZE=64
-readonly EPOCHS=10
+readonly EPOCHS=20
 readonly DROPOUT_RATE=0.5
 readonly DENSE_UNITS=128
 readonly EVAL_STEPS=500
